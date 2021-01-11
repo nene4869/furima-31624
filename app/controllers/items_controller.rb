0e8ee6a4ae048e406dda80e3f60645ddd718c474
nene <48694062nene@gmail.com>
2021-01-11 15:50:17 +0900

    commit2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :show, :update]
+  
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -20,20 +21,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-  
-   @item = Item.find(params[:id])
+   end
 
-  end
-
-  def edit#ログインしているユーザーのidとアイテムに紐づいているユーザーのidが一致しているかの条件式
-    @item = Item.find(params[:id])
+  def edit
     if @item.user.id != current_user.id
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     if @item.update(item_params)
     redirect_to action: :show
   else
@@ -53,13 +50,9 @@ class ItemsController < ApplicationController
     unless user_signed_in?
       redirect_to action: :index
     end
+ end
 
-
-
-   end
-  
-   
-   
-
-
+ def set_item
+  @item = Item.find(params[:id])
+ end
 end
